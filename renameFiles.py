8c9244f0e8cef2087da6fabec32152b200dcfa61
tfishbeck95 <tyfishbeck51@gmail.com>
2019-10-02 04:01:24 -0500

    sd

---
+++ renameFiles.py
@@ -0,0 +1,13 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio Version 16
+VisualStudioVersion = 16.0.29326.143
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {3F7B9173-1C1E-4BA0-9CAE-0575D692999B}
+	EndGlobalSection
+EndGlobal
